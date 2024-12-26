9b847be9945b60c4b9a25b5201d128499eb2c528
Arjun Nair~ <arjunnair101105@gmail.com>
2024-12-26 15:36:11 +0530

    Image Carousel Done

---
+++ src/Carousel.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel"
+
+const ImageCarousel = ()=>{
+    
+    return(
+        <div className="w-[90%] max-w-[800px] mx-auto">
+            <Carousel 
+            showThumbs={false}
+            autoPlay={true}
+            infiniteLoop={true}
+            showStatus={false}
+            interval={3000}
+            transitionTime={500}
+            >
+                <div>
+                    <img src="/images/bikeRide1.jpeg" alt="Bike Ride Image" />
+                    <p className="legend">CMV Bike Ride 2023</p>
+                </div>
+                <div>
+                    <img src="/images/newYear1.jpeg" alt="New Year Image" />
+                    <p className="legend">CMV New Year 2023</p>
+                </div>
+                <div>
+                    <img src="/images/bikeRide2.jpeg" alt="Bike Ride Image" />
+                    <p className="legend">CMV Bike Ride 2023</p>
+                </div>
+                <div>
+                    <img src="/images/trekImg.jpeg" alt="Trek Image" />
+                    <p className="legend">CMV Trek 2023</p>
+                </div>
+                <div>
+                    <img src="/images/newYear2.jpeg" alt="New Year Image" />
+                    <p className="legend">CMV New Year 2023</p>
+                </div>
+                <div>
+                    <img src="/images/bikeRide3.jpeg" alt="Bike Ride Image" />
+                    <p className="legend">CMV Bike Ride 2022</p>
+                </div>
+            </Carousel>
+        </div>
+    )
+    
+}
+export default ImageCarousel;
